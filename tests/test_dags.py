74dbfe852971af286fcd556b28decf853081320a
Gerson <gersonrodriguessantos8@gmail.com>
2025-02-10 02:54:00 -0300

    feat: new workflow

--- tests/test_dags.py
+++ tests/test_dags.py
@@ -4,4 +4,4 @@ from airflow.models import DagBag
 
 def test_dag_integrity():
     dag_bag = DagBag(dag_folder="dags", include_examples=False)
-    assert dag_bag.import_erros == {}, "DAGs com erro de importação"
+    assert dag_bag.import_errors == {}, "DAGs com erro de importação"
